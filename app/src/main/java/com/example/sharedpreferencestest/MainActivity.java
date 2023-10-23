87c83035fdd74dc1da1970224be855ffce66558f
guocz <2589945540@qq.com>
2023-10-23 14:08:17 +0800

    使用SharedPreferences存数据

---
+++ app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.sharedpreferencestest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button saveData = findViewById(R.id.save_data);
+        saveData.setOnClickListener(v -> {
+            SharedPreferences.Editor edit = getSharedPreferences("data", MODE_PRIVATE).edit();
+            edit.putString("name", "Tom");
+            edit.putInt("age", 18);
+            edit.putBoolean("marry", false);
+            edit.apply();
+        });
+    }
+}
